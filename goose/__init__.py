cf6f4021cb43450f4f17dd5c76ba4194118f3123
Ryan <remenecker@wustl.edu>
2025-05-07 08:42:34 -0500

    updated install to pyproject.toml

--- goose/__init__.py
+++ goose/__init__.py
@@ -1,18 +1,18 @@
 """A Python package for making IDRs and IDR variants"""
 
 # Add imports here
-from .create import *
-from .analyze import *
-from .optimize import *
-from .backend.optimizer_properties import *
+from goose.create import *
+from goose.analyze import *
+from goose.optimize import *
+from goose.backend.optimizer_properties import *
 import os 
 
-# Handle versioneer
-from ._version import get_versions
-versions = get_versions()
-__version__ = versions['version']
-__git_revision__ = versions['full-revisionid']
-del get_versions, versions
+# Generate _version.py if missing and in the Read the Docs environment
+if os.getenv("READTHEDOCS") == "True" and not os.path.isfile('../goose/_version.py'):   
+    import versioningit            
+    __version__ = versioningit.get_version('../')
+else:
+    from ._version import __version__
 
 # code that allows access to the data directory
 _ROOT = os.path.abspath(os.path.dirname(__file__))
