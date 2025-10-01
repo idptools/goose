00fc39d30cfc89b3cec5f4126fc1c5f18a2f1ca4
Ryan <remenecker@wustl.edu>
2025-10-01 07:31:36 -0500

    update of cythonized code

--- goose/backend/fast_mutations.c
+++ goose/backend/fast_mutations.c
@@ -5,7 +5,7 @@
     "distutils": {
         "depends": [],
         "include_dirs": [
-            "/private/var/folders/ww/r3d5hbss3vq8yxhqzqjv_p4w0000gn/T/pip-build-env-h_47ky6_/overlay/lib/python3.11/site-packages/numpy/_core/include"
+            "/private/var/folders/ww/r3d5hbss3vq8yxhqzqjv_p4w0000gn/T/build-env-dx8fwkla/lib/python3.8/site-packages/numpy/core/include"
         ],
         "name": "goose.backend.fast_mutations",
         "sources": [
